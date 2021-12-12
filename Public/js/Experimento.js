74df6be2a76a17cacce99c3bd4e87a54b520cd1a
Monã Santos <monagabriel30@gmail.com>
2021-12-12 19:51:30 -0300

    Nikel

---
+++ Public/js/Experimento.js
@@ -0,0 +1,52 @@
+pessoadefault = {
+    nome:"Monã Santos",
+    Idade:"18",
+    Trabalho:"Programador"
+}
+
+let nomes  = ["Marccelo Eltz","Maria Silva", "Pedrom silva"];
+let pessoaas = [
+    
+    {
+        nome:"Monã Santos",
+        Idade:"18",
+        Trabalho:"Programador"
+    },
+    {
+        nome:"Maria Silva",
+        Idade:"25",
+        Trabalho:"Programadora"
+    }
+];
+
+function adicionarpessoa(pessoa) {
+    pessoas.push(pessoa)
+}
+
+
+
+
+console.log(pessoas);
+
+
+console.log(pessoa);
+
+console.log("nome:");
+console.log(pessoa.nome);
+console.log("Idade:");
+console.log(pessoa.Idade);
+console.log("Trabalho:");
+console.log(pessoa.Trabalho);
+
+function imprimirpessoa(pessoa){
+console.log("nome:");
+console.log(pessoa.nome);
+console.log("Idade:");
+console.log(pessoa.Idade);
+console.log("Trabalho:");
+console.log(pessoa.Trabalho);
+}
+
+newFunction();
+
+console.log(nomes [2]);
